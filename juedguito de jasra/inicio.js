cb7941db69c697de8b474d105c244b878f574d67
JamelynRoxanaOlivares <123043894+JamelynRoxanaOlivares@users.noreply.github.com>
2023-06-28 22:16:49 -0600

    portada
    solo fata cambiar el boton a cofre

---
+++ juedguito de jasra/inicio.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+    var button = document.getElementById("myButton");
+    button.style.backgroundImage = "url('cofre.gif')";
+    button.style.backgroundSize = "cover";
+    button.style.backgroundRepeat = "no-repeat";
+    button.style.animation = "none";
+}
