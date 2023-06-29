b3e9a184304095713414b3d147b9c8273a29c81a
Oscar Mata <118559087+JDFenix@users.noreply.github.com>
2023-06-29 01:32:43 -0600

    Delete juedguito de jasra directory

--- juedguito de jasra/inicio.js
+++
@@ -1,7 +0,0 @@
-function changeBackground() {
-    var button = document.getElementById("myButton");
-    button.style.backgroundImage = "url('cofre.gif')";
-    button.style.backgroundSize = "cover";
-    button.style.backgroundRepeat = "no-repeat";
-    button.style.animation = "none";
-}
